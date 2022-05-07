b32a111c649434b246c9fb1a3b495582e614e64d
smzahurul <zahurul.it@gmail.com>
2022-05-07 19:22:48 +0600

    first commit

---
+++ letconst.js
@@ -0,0 +1,21 @@
+// dont Changer 
+const hubby = "Elias Kanchon";
+// hubby = "Omar sunny"
+// console.log(hubby);
+
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(77);
+// numbers = ["sunny", "sakib"];
+// console.log(numbers);
+
+// changer Values
+
+let patientName = "Rahim Chacha";
+patientName = "Fatema Khala";
+console.log(patientName);
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    sum = sum + i;
+    console.log(sum);
+}
