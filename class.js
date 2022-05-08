f8601d9204ef7493dc1e88bf90ec2a94c2d29a58
smzahurul <zahurul.it@gmail.com>
2022-05-08 21:31:58 +0600

    Six Project Finish

--- class.js
+++ class.js
@@ -1,4 +1,12 @@
 // Class, constructor, create object from class
 class Student {
-
+    constructor(sId, sName, sAge) {
+        this.id = sId;
+        this.name = sName;
+        this.roll = sAge;
+        this.school = "SamasPara B/L High School";
+    }
 }
+const student1 = new Student(1, "Rahim", 17);
+const student2 = new Student(2, "Rabbil", 16);
+console.log(student1, student2);
