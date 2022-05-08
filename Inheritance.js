842beebd69cf07276bda2c8ff8afd2a256341be7
smzahurul <zahurul.it@gmail.com>
2022-05-08 21:51:52 +0600

    Seven Project Finish

---
+++ Inheritance.js
@@ -0,0 +1,19 @@
+// Inheritance, extends class, super, class method
+class Parent {
+    constructor() {
+        this.fatherName = "Schwerznegger";
+    }
+}
+
+class Child extends Parent {
+    constructor(name) {
+        super();
+        this.name = name;
+    }
+    getFullName() {
+        return this.name + " " + this.fatherName;
+    }
+}
+const baby = new Child('Arnold');
+const baby2 = new Child('Tom');
+console.log(baby.getFullName(), baby2.getFullName());
